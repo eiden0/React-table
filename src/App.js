7c0aa418c1ea5f436afe50eccccb9fa2225ca289
Eiden0 <shibashis12@gmail.com>
2022-01-19 12:45:50 +0530

    prettify code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import data from "./data.json"
+import data from "./data.json";
 import React from "react";
 import styles from "./App.module.css";
 import Table from "./components/Table";
 
 const App = () => {
-  const {headers, rows} = data;
+  const { headers, rows } = data;
   return (
     <main className={styles.container}>
       <header>
         <p>React Table Component</p>
       </header>
       <div className={styles.wrapper}>
-        <Table headers = {headers} rows={rows} rowsPerPage={5} />
+        <Table headers={headers} rows={rows} rowsPerPage={5} />
       </div>
     </main>
   );
--- src/components/Table/TableFooter/index.jsx
+++ src/components/Table/TableFooter/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-
 import styles from "./TableFooter.module.css";
 
 const TableFooter = ({ range, setPage, page, slice }) => {
